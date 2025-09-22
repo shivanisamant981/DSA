3c25f14fba54fab732037186abeb49beb5ac2719
shivani <shivanisamant981@gmail.com>
2025-09-22 10:59:09 +0530

    3 question done

--- string_3_.cpp
+++ string_3_.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+//Hi Anonymous what you doing here are you doing good yes okay that's great  
 vector<int> findLPS(string s){
             int n=s.size();
             vector<int> lps(n,0);
@@ -26,23 +26,37 @@ vector<int> findLPS(string s){
             }
             return lps;
 }
+bool kmp(string s,string pat,vector<int> lps ){
+    int m=pat.size();
+    int n=s.size();
+    int i=0;
+    int j=0;
+    while(i<n){
+        if(s[i]==pat[j]){
+            i++;
+            j++;
+        }
+        if (j==m){
+            return true;
+        }
+        else if(s[i]!=pat[j]){
+            if(j!=0){
+                j=lps[j-1];
+            }
+            else{
+                i++;
+            }
 
-bool kmp(string s,vector<int> ){
-    
-
+        }
+    }
+    return false;
 }
-
-
 int main(){
          
     string s="abababd";
     string patt="ababd";
-    vector<int> lps=findLPS(s);
-    for(auto value:lps){
-        cout<<value<<" ";
-    }
-
-
-
+    vector<int> lps=findLPS(patt);
+    bool res=kmp(s , patt, lps);
+    cout<<res;
     return 0;
 }
